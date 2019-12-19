9aa29f1a5dc4804068bfa5674aec147d27e9b925
VGhazaryan <49144300+VGhazaryan@users.noreply.github.com>
2019-12-19 12:03:58 +0400

    added function

--- h3.py
+++ h3.py
@@ -3,17 +3,20 @@ import datetime
 
 file = open('text.txt')
 allDates = []
+
+def checkDate(date):
+    isDate = True
+    day, month, year = date.split('/')
+    try:
+        datetime.datetime(int(year), int(month), int(day))
+    except ValueError:
+        isDate = False
+    return isDate
+
 for i in file:
-    print(file)
     dates = re.findall(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", i)
     print(dates)
     for d in dates:
-        isDate = True
-        day, month, year = d.split('/')
-        try:
-            datetime.datetime(int(year), int(month), int(day))
-        except ValueError:
-            isDate = False
-        if isDate:
+        if checkDate(d):
             allDates.append(d)
 print(allDates)
